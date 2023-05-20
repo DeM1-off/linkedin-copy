58e0e7e8367a18c711f2144e8baa611edaf745e6
Dmitro Dohryk <dmitro.dohryk@gmail.com>
2023-05-20 09:25:42 +0300

    Initial Commit

---
+++ linkedin/linkedinApp.swift
@@ -0,0 +1,17 @@
+//
+//  linkedinApp.swift
+//  linkedin
+//
+//  Created by Dmitro Dohryk on 20.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct linkedinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
